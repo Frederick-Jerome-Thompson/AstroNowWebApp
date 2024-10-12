a7ccefc5f37bf1b4e95365c50123cac1cf1f2e16
Frederick-Jerome-Thompson <46221343+Frederick-Jerome-Thompson@users.noreply.github.com>
2024-10-12 16:32:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -173,7 +173,7 @@ def getData():
       theHereTime = astroNow.hereTime_inpt(lat=float(reqLat),lon=float(reqLon),
 					year=reqTime.year, month=reqTime.month, day=reqTime.day,
 					hour=reqTime.hour, minute=reqTime.minute, second=reqTime.second)
-      theNow = theHereTime.astimezone(pytz.utc)
+      theNow = astrNow.utcTime_inpt(reqTime,timezone)
     
     QR.date = theNow
     
